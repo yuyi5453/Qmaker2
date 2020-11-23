6f646712784e82e550725927d66a6b1c8a9b641b
yuyi5453 <744530689@qq.com>
2020-11-23 13:14:17 +0800

    1

--- src/main/java/action/LoginAction.java
+++ src/main/java/action/LoginAction.java
@@ -3,10 +3,39 @@ package action;
 import com.opensymphony.xwork2.ActionSupport;
 import org.omg.PortableInterceptor.SUCCESSFUL;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import service.UserService;
 
+import javax.xml.ws.soap.Addressing;
+
+@Component
 public class LoginAction extends ActionSupport {
-    
+    public UserService getUserService() {
+        return userService;
+    }
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Autowired
     UserService userService;
     String userId;
     String password;
